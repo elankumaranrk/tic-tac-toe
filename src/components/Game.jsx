6dd9b9212ea3f3437e2fc00317e60c5b979db9e3
Elankumaran Kuppusamy <elankumaran.ramasamykuppusamy@concur.com>
2018-08-08 09:02:39 -0500

    Add ai's game play

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,7 @@ import Status from './Status';
 import Board from './Board';
 import Controls from './Controls';
 import BoardLogic from '../lib/Board';
+import TicTacToe from '../lib/TicTacToe';
 
 export default class Game extends Component {
 	state = {
@@ -17,17 +18,42 @@ export default class Game extends Component {
 		const { value } = target;
 		this.setState({ mode: value });
 	};
+	aiGamePlay = () => {
+		const { mode, cells } = this.state;
+		switch (mode) {
+			case 'easy': {
+				return new TicTacToe().getNextEasyMove(cells);
+			}
+			case 'medium': {
+				return new TicTacToe().getNextMediumMove(cells);
+			}
+			case 'hard': {
+				return new TicTacToe().getNextBestMove(cells);
+			}
+		}
+	};
 	cellClickHandler = cellIndex => {
-		const { cells, isXNext } = this.state;
+		const { cells, isXNext, mode } = this.state;
 
 		let newCells = cells.slice();
 		newCells[cellIndex] = isXNext ? 'X' : 'O';
 
-		this.setState({ cells: newCells, isXNext: !isXNext });
-		this.checkWinner(newCells);
+		this.setState({ cells: newCells, isXNext: !isXNext }, () => {
+			this.checkWinner();
+			if (mode !== 'faf') {
+				setTimeout(() => {
+					const newAiMove = this.aiGamePlay();
+					let aiCells = newCells.slice();
+					aiCells[newAiMove] = 'O';
+					this.setState({ cells: aiCells, isXNext: true });
+				}, 400);
+			}
+			this.checkWinner();
+		});
 	};
 
-	checkWinner(cells) {
+	checkWinner() {
+		const { cells } = this.state;
 		return new Promise(resolve => setTimeout(resolve, 200)).then(() => {
 			const board = new BoardLogic(cells);
 			const winner = board.isTerminal();
